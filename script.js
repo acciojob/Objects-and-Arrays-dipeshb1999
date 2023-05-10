410d69dba0d1fe9393b1724c5415cf6b3f62e212
Badgujar Dipesh <117906055+dipeshb1999@users.noreply.github.com>
2023-05-10 16:49:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,31 +1,33 @@
-const players = ["John", "Bob", "Alice", "Poppy"];
+let players = ["Player 1", "Player 2", "Player 3"];
 
-const person = {
+let person = {
   name: "John Doe",
-  age: 80,
-};
-// Given array
-var players = ["John", "Mike", "Sarah", "Tom"];
-
-// Given object
-var person = {
-  name: "Alex",
   age: 25,
 };
 
-// Create a reference to players array
-var team = players;
+let team = players;
+let team1 = [...players];
+let cap1 = { ...person };
+let players = ["Player 1", "Player 2", "Player 3"];
 
-// Create a copy of players array
-var team1 = players.slice();
+let person = {
+  name: "John Doe",
+  age: 25,
+};
 
-// Create a copy of person object
-var cap1 = Object.assign({}, person);
+let team = players;
+let team1 = [...players];
+let cap1 = { ...person };
 
-// Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
+console.log(team);  // Output: ["Player 1", "Player 2", "Player 3"]
+console.log(team1); // Output: ["Player 1", "Player 2", "Player 3"]
+console.log(cap1);  // Output: { name: "John Doe", age: 25 }
+players.push("Player 4");
+person.age = 30;
 
-window.players = players;
-window.person = person;
-window.team = team;
-window.team1 = team1;
-window.cap1 = cap1;
+console.log(team);  // Output: ["Player 1", "Player 2", "Player 3", "Player 4"]
+console.log(team1); // Output: ["Player 1", "Player 2", "Player 3"]
+console.log(cap1);  // Output: { name: "John Doe", age: 25 }
+cap1.name = "Jane Smith";
+console.log(cap1);  // Output: { name: "Jane Smith", age: 25 }
+console.log(person); // Output: { name: "John Doe", age: 25 }
