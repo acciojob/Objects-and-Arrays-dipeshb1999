27396a3ae3cce735fe931ba9b4ec6a77df37d920
Badgujar Dipesh <117906055+dipeshb1999@users.noreply.github.com>
2023-05-10 17:04:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,33 +1,14 @@
-let players = ["Player 1", "Player 2", "Player 3"];
+var players = ["Player 1", "Player 2", "Player 3"];
 
-let person = {
+var person = {
   name: "John Doe",
-  age: 25,
+  age: 25
 };
 
-let team = players;
-let team1 = [...players];
-let cap1 = { ...person };
-let players = ["Player 1", "Player 2", "Player 3"];
+var team = players;
+var team1 = players.slice();
+var cap1 = { ...person };
 
-let person = {
-  name: "John Doe",
-  age: 25,
-};
-
-let team = players;
-let team1 = [...players];
-let cap1 = { ...person };
-
-console.log(team);  // Output: ["Player 1", "Player 2", "Player 3"]
-console.log(team1); // Output: ["Player 1", "Player 2", "Player 3"]
-console.log(cap1);  // Output: { name: "John Doe", age: 25 }
-players.push("Player 4");
-person.age = 30;
-
-console.log(team);  // Output: ["Player 1", "Player 2", "Player 3", "Player 4"]
-console.log(team1); // Output: ["Player 1", "Player 2", "Player 3"]
-console.log(cap1);  // Output: { name: "John Doe", age: 25 }
-cap1.name = "Jane Smith";
-console.log(cap1);  // Output: { name: "Jane Smith", age: 25 }
-console.log(person); // Output: { name: "John Doe", age: 25 }
+console.log(team);   // Output: ["Player 1", "Player 2", "Player 3"]
+console.log(team1);  // Output: ["Player 1", "Player 2", "Player 3"]
+console.log(cap1);   // Output: { name: "John Doe", age: 25 }
